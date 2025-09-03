02031e538f22c6468bc5fca0019a64ddcefe4fbd
grossj0816 <juwangross0815@gmail.com>
2025-09-03 14:00:22 -0400

    Added calendar component and grid code blocks to grid out content on screen

--- task-commander/src/App.js
+++ task-commander/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/header';
 import 'primereact/resources/themes/viva-dark/theme.css';
 import "/node_modules/primeflex/primeflex.css";
+import UserCalendar from './components/calendar';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
             </div>
             <div className="col-7 col-offset-1">
               <div className="text-center p-3 border-round-sm bg-primary font-bold">
-                
+                <UserCalendar />
               </div>    
             </div>
             <div className="col-2">
---
+++ task-commander/src/components/calendar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UserCalendar({}){
+    const currentDate = new Date();
+    return(
+        <>
+        </>
+    );
+}
