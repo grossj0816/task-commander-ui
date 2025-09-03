bd8ad8f223c9f1aa5164394460418561ebb6ce17
grossj0816 <juwangross0815@gmail.com>
2025-09-03 11:27:07 -0400

    Added code to grid out responsive web content in App.js

--- task-commander/src/App.js
+++ task-commander/src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import 'primereact/resources/themes/viva-dark/theme.css';
+import "/node_modules/primeflex/primeflex.css";
 
 function App() {
   return (
     <div className="App">
-      
+        <Header />
+        <div className="grid">
+            <div className="col-2">
+              <div className="text-center p-3 border-round-sm bg-primary font-bold">2</div>    
+            </div>
+            <div className="col-7 col-offset-1">
+              <div className="text-center p-3 border-round-sm bg-primary font-bold">
+                
+              </div>    
+            </div>
+            <div className="col-2">
+              <div className="text-center p-3 border-round-sm bg-primary font-bold">2</div>    
+            </div>
+        </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
